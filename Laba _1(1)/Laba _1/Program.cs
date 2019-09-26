256b5ac9f15146f38d14080887f8778e3190e2eb
Alex Vak <mamorunokitsune@gmail.com>
2019-09-26 23:43:49 +0300

    Laba1 v1.0

---
+++ Laba _1(1)/Laba _1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba__1
+{
+    class Adress
+    {
+        public string country { get; set; } = "England";
+        public string city { get; set; } = "London";
+        public string street { get; set; } = "Baker Street";
+        public string house { get; set; } = "221B";
+        public int apartment { get; set; } = 2;
+        public string index { get; set; } = "NW1 5LA";
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Adress HolmesAdress = new Adress();
+            Console.WriteLine("{0}, {1}, {2}, {3}, {4} apartment, index : {5}", HolmesAdress.country, HolmesAdress.city, HolmesAdress.street, HolmesAdress.house, HolmesAdress.apartment, HolmesAdress.index);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Laba1(4)/Laba1(4)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba1_4_
+{
+    class Program
+    {
+        class User
+        {
+            public string login;
+            public string password;
+            public int age;
+            string date_of_registration;
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
