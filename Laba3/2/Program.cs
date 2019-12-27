eb5049b902b2741f4aac982f5b0f6ec8079113f5
Alex Vak <mamorunokitsune@gmail.com>
2019-12-27 22:47:49 +0200

    Laba3

---
+++ Laba3/2/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    interface IPlayable
+    {
+        void Play();
+        void Pause();
+        void Stop();
+
+    }
+    interface IRecordable
+    {
+        void Record();
+        void Pause();
+        void Stop();
+    }
+    class Player : IPlayable, IRecordable
+    {
+        public void Play()
+        {
+            Console.WriteLine("Track is playing");
+        }
+        public void Record()
+        {
+            Console.WriteLine("Sound is recording");
+        }
+        void IPlayable.Pause()
+        {
+            Console.WriteLine("Track is paused");
+        }
+        void IPlayable.Stop() 
+        {
+            Console.WriteLine("Track is stopped");
+        }
+        void IRecordable.Pause()
+        {
+            Console.WriteLine("Record is paused");
+        }
+        void IRecordable.Stop()
+        {
+            Console.WriteLine("Record is stopped");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isFinished = false;
+            bool isPlayFinished = false;
+            bool isRecordFinished = false;
+            while (isFinished == false)
+            {
+                Console.WriteLine("Please choose one menu : Play - Playing menu; Record - Recording menu" +
+                    "\nIf you want to finish enter Finish");
+                string input = Console.ReadLine().Trim();
+                switch (input)
+                {
+                    case "Play":
+                        {
+                            while(isPlayFinished == false)
+                            {
+                                IPlayable playing = new Player();
+                                Console.WriteLine("Welcome to playing menu." +
+                                    "\nIf you want to play track enter Play" +
+                                    "\nIf you want to pause track enter Pause" +
+                                    "\nIf you want to stop track enter Stop" +
+                                    "\nBack to main menu enter b");
+                                string playInput = Console.ReadLine().Trim();
+                                switch (playInput)
+                                {
+                                    case "Play":
+                                        {
+                                            Console.WriteLine(("").PadRight(40, '-'));
+                                            playing.Play();
+                                            Console.WriteLine(("").PadRight(40, '-'));
+                                        }
+                                        break;
+                                    case "Pause":
+                                        {
+                                            Console.WriteLine(("").PadRight(40, '-'));
+                                            playing.Pause();
+                                            Console.WriteLine(("").PadRight(40, '-'));
+                                        }
+                                        break;
+                                    case "Stop":
+                                        {
+                                            Console.WriteLine(("").PadRight(40, '-'));
+                                            playing.Stop();
+                                            Console.WriteLine(("").PadRight(40, '-'));
+                                        }
+                                        break;
+                                    case "b":
+                                        {
+                                            isPlayFinished = true;
+                                        }
+                                        break;
+                                        
+                                }
+                            }
+                        }
+                        break;
+                    case "Record":
+                        {
+                            while (isRecordFinished == false)
+                            {
+                                IRecordable recording = new Player();
+                                Console.WriteLine("Welcome to recording menu." +
+                                   "\nIf you want to record sound enter Play" +
+                                   "\nIf you want to pause recording enter Pause" +
+                                   "\nIf you want to stop recording enter Stop" +
+                                   "\nBack to main menu enter b");
+                                string recordInput = Console.ReadLine().Trim();
+                                switch (recordInput)
+                                {
+                                    case "Play":
+                                        {
+                                            Console.WriteLine(("").PadRight(40, '-'));
+                                            recording.Record();
+                                            Console.WriteLine(("").PadRight(40, '-'));
+                                        }
+                                        break;
+                                    case "Pause":
+                                        {
+                                            Console.WriteLine(("").PadRight(40, '-'));
+                                            recording.Pause();
+                                            Console.WriteLine(("").PadRight(40, '-'));
+                                        }
+                                        break;
+                                    case "Stop":
+                                        {
+                                            Console.WriteLine(("").PadRight(40, '-'));
+                                            recording.Stop();
+                                            Console.WriteLine(("").PadRight(40, '-'));
+                                        }
+                                        break;
+                                    case "b":
+                                        {
+                                            isRecordFinished = true;
+                                        }
+                                        break;
+                                }
+                            }
+
+                        }
+                        break;
+                    case "Finish":
+                        {
+                            isFinished = true;
+                        }
+                        break;
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Laba3/3(Test)/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_Test_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxCount = 0;
+
+            int[] temperature = new int[4];
+            int[] jmax = new int[4];
+            int[] date = new int[5];
+            int[,] table = {
+                { -5, -10, -3, -3 },
+                { -3, -7, -8, -6 },
+                { -5, 2, 2, 2  }
+            };
+            Console.WriteLine(table.GetLength(1));
+            int count = 0;
+            for (int i = 0; i < table.GetLength(1); i++)
+            {
+                count = 0;
+                for (int j = 0; j < table.GetLength(0); j++)
+                {
+                    if (table[i, j] == table[i, j + 1])
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        if (count > maxCount)
+                        {
+                            maxCount = count;
+                            temperature[i] = table[i, j];
+                            date[j] = j++;
+                        }
+
+
+                    }
+                    count = 0;
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
